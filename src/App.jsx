f4f0db5dfb9cda4a128da394ad14f667a6101b37
jrojas931022 <jjru931022@gmail.com>
2024-10-07 18:04:37 -0500

    crud array people finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
 import { useState } from 'react'
-
 import './App.css'
-import { Person1 } from './components/Person1';
-import { People1 } from './components/People1';
+import { People } from './components/People';
 
 function App() {
-  const [people1, setPeople1] = useState([
+
+  const [people, setPeople] = useState([
     {
-       id: 1,
-       name: "Inés Oliveros",
-       role: "Backend Developer",
-       img: "https://bootdey.com/img/Content/avatar/avatar8.png"
+      id: 1,
+      name: "Inés Oliveros",
+      role: "Backend Developer",
+      img: "https://bootdey.com/img/Content/avatar/avatar8.png"
     },
     {
-       id: 2,
-       name: "Francisco Perez",
-       role: "Designer",
-       img: "https://bootdey.com/img/Content/avatar/avatar5.png"
+      id: 2,
+      name: "David Romero",
+      role: "QA",
+      img: "https://bootdey.com/img/Content/avatar/avatar2.png"
     },
     {
-       id: 3,
-       name: "Natalia Zuliaga",
-       role: "frontend",
-       img: "https://bootdey.com/img/Content/avatar/avatar3.png"
+      id: 3,
+      name: "Karen Steffany",
+      role: "Frontend Developer",
+      img: "https://bootdey.com/img/Content/avatar/avatar3.png"
     }
- ]);
- 
+  ]);
 
   return (
     <div>
-    <People1
-    people1={people1}
-    setPeople1={setPeople1}
-    />   
-    <Person1 />   
-
+      <People
+        people={people}
+        setPeople={setPeople}
+      />
     </div>
   )
 }
 
-export default App
+export default App;
